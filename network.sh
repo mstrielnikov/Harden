bf3a1dbd0cec1e9a4c48e505dd4ed195bc85d128
mstrielnikov <37501284+mstrielnikov@users.noreply.github.com>
2020-10-08 20:29:27 +0300

    Create network.sh

---
+++ network.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/sh
+
+cat >>/etc/sysctl.conf <<EOF
+net.ipv4.icmp_echo_ignore_broadcasts = 1
+net.ipv4.icmp_ignore_bogus_error_responses = 1
+net.ipv4.tcp_syncookies = 1
+net.ipv4.conf.all.accept_source_route = 0
+net.ipv4.conf.default.accept_source_route = 0
+net.ipv4.conf.all.rp_filter = 1
+net.ipv4.conf.default.rp_filter = 1
+net.ipv4.ip_forward = 0
+net.ipv4.conf.all.send_redirects = 0
+net.ipv4.conf.default.send_redirects = 0
+kernel.exec-shield = 1
+kernel.randomize_va_space = 1
+net.ipv4.ip_local_port_range = 2000 65000
+net.ipv4.tcp_rmem = 4096 87380 8388608
+net.ipv4.tcp_wmem = 4096 87380 8388608
+net.core.rmem_max = 8388608
+net.core.wmem_max = 8388608
+net.core.netdev_max_backlog = 5000
+net.ipv4.tcp_window_scaling = 1
+EOF
+
+exit 0;
